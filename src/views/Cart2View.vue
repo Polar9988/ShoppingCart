0a582324cb787f96e012287aa3ab96680dc9d067
Polar9988 <jeffery110344@gmail.com>
2024-08-09 15:16:00 +0800

    test15

--- src/views/Cart2View.vue
+++ src/views/Cart2View.vue
@@ -185,9 +185,9 @@ export default {
                                     <div
                                         class="col-3 col-lg-3 align-items-center d-flex text-center justify-content-center">
                                         <div class="">
-                                            <p class="mb-0">{{ shoppingList[0].quantity }} + {{ shoppingList[1].quantity
-                                                }} + {{ shoppingList[2].quantity }} = {{ shoppingList[0].quantity +
-                                                    shoppingList[1].quantity + shoppingList[2].quantity }}</p>
+                                            <p class="mb-0" v-if="shoppingList.length >= 3">
+                                                {{ totalQuantity }}
+                                            </p>
                                             <p class="mb-0" v-if="shoppingList.length >= 3">
                                                 $ {{ subtotal }}
                                             </p>
