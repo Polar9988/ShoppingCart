824938257c7fb4f2987e6abdaee830a524bfc4d8
Polar9988 <jeffery110344@gmail.com>
2024-08-09 14:26:32 +0800

    test5

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,6 @@ export default {
 </script>
 
 <template>
-
-  <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./BS5-example2.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  </head>
   <header>
     <nav class="navbar navbar-expand-lg "
       style="background:linear-gradient(to right, #e4afcb 0%, #b8cbb8 0%, #b8cbb8 0%, #e2c58b 30%, #c2ce9c 64%, #7edbdc 100%);">
