0040a164aac82d980258a47c578524586161b76d
Polar9988 <jeffery110344@gmail.com>
2025-02-24 21:26:59 +0800

    sign in fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -45,7 +45,11 @@ export default {
                         Remember me
                     </label>
                 </div>
-                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <RouterLink to="/cart1">
+                    <button class="btn btn-primary w-100 py-2" type="submit">
+                        Sign in
+                    </button>
+                </RouterLink>
                 <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
             </form>
         </main>
